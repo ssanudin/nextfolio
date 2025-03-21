29b7e66ad0c6e663aa52e83bff44f82192e4906b
Sanudin <mailto.sanudin@gmail.com>
2025-03-22 06:08:26 +0700

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,31 @@ import "@/app/ui/globals.css";
 export const metadata: Metadata = {
   title: "Sanudin | Software Engineer and Developer",
   description:
-    "A software engineer and developer passionate about helping people to create great products.",
+    "A software engineer and developer passionate about helping entrepreneurs create great products.",
+  authors: [{ name: "Sanudin", url: "https://sanud.in" }],
+  keywords: [
+    "sanudin",
+    "ssanudin",
+    "software engineer",
+    "developer",
+    "web developer",
+    "nextjs",
+    "portfolio website",
+    "portfolio site",
+  ],
+  openGraph: {
+    title: "Sanudin | Software Engineer and Developer",
+    description:
+      "Passionate about helping entrepreneurs create great products.",
+    images: "/og.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sanudin | Software Engineer and Developer",
+    description:
+      "Passionate about helping entrepreneurs create great products.",
+    images: "/og.png",
+  },
 };
 
 export default function RootLayout({
